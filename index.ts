af7e1b63eddaa2d10747bbcd1895a3a56802d46d
aynp <aryan.pathania2003@gmail.com>
2022-06-19 07:26:47 +0530

    Refactoring

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use("/api/", router);
+
+export default app;
